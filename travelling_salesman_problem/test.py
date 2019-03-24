b3b4be22e01049bb53629860f13b56ceda3870fc
TomaszKot11 <kottomasz98@gmail.com>
2019-03-24 23:58:11 +0100

    Added sample program flow

--- travelling_salesman_problem/test.py
+++ travelling_salesman_problem/test.py
@@ -14,6 +14,49 @@ from AAsteriskAlgorithm import AAsteriskAlgorithm
 
 # print('with fixed cities:')
 
+def print_result(result):
+  for key, value in result.items():
+    print(str(key) + ' ' + str(value))
+
+def perform_sample_algorithm(first_city_storage):
+  bfs_algorithm = BfsBruteForce(first_city_storage)
+  bfs_result = bfs_algorithm.perform()
+  print('BFS algorithm')
+  print_result(bfs_result)
+
+  print('DFS algorithm')
+  dfs_algorithm = DfsBruteForce(first_city_storage)
+  dfs_result = dfs_algorithm.perform()
+
+  print_result(dfs_result)
+
+  print('Ordinary brute force:')
+  ordinary_brute_force_algorithm = OrdinaryBruteForce(first_city_storage)
+  ordinary_brute_result = ordinary_brute_force_algorithm.perform()
+  print_result(ordinary_brute_result)
+
+  print('NN algorithm:')
+  nn_algorithm = NNAlgorithm(first_city_storage)
+  nn_algorithm_result = nn_algorithm.perform()
+  print_result(nn_algorithm_result)
+
+  print('A* algorithm:')
+  aa_algorithm = AAsteriskAlgorithm(first_city_storage)
+  aa_algorithm_result = aa_algorithm.perform()
+  print_result(aa_algorithm_result)
+
+  PlotDrawer(city_storage = first_city_storage, cities = dfs_result['cities'], title = 'Brute force DFS algorithm')
+  PlotDrawer(city_storage = first_city_storage, cities = bfs_result['cities'], title = 'Brute force BFS algorithm')
+  PlotDrawer(city_storage = first_city_storage, cities = ordinary_brute_result['cities'], title = 'Ordinary brute force')
+  PlotDrawer(city_storage = first_city_storage, cities = nn_algorithm_result['cities'], title = 'NN algorithm')
+  PlotDrawer(city_storage = first_city_storage, cities = aa_algorithm_result['cities'], title = 'A* algorithm')
+
+
+def print_delimeters():
+  print('---------')
+  print('---------')
+  print('---------')
+  print('---------')
 
 
 first_test_array = [
@@ -33,44 +76,21 @@ second_test_array = [
   [3, 93, 68],
   [4, 45, 67],
   [5, 56, 43],
-  [6, 32, 12]
+  [6, 32, 12],
+  [7, 12, 17],
+  [8, 78, 14]
 ]
 
-def print_result(result):
-  for key, value in result.items():
-    print(str(key) + ' ' + str(value))
 
-first_city_storage = CityStorage.with_given_cities(first_test_array)
+my_first_city_storage = CityStorage.with_given_cities(first_test_array)
+my_second_city_storage = CityStorage.with_given_cities(second_test_array)
+my_random_city_storage = CityStorage(10)
+
+perform_sample_algorithm(my_first_city_storage)
+print_delimeters()
+perform_sample_algorithm(my_second_city_storage)
+print_delimeters()
+perform_sample_algorithm(my_random_city_storage)
+print_delimeters()
 
 
-bfs_algorithm = BfsBruteForce(first_city_storage)
-bfs_result = bfs_algorithm.perform()
-print('BFS algorithm')
-print_result(bfs_result)
-
-print('DFS algorithm')
-dfs_algorithm = DfsBruteForce(first_city_storage)
-dfs_result = dfs_algorithm.perform()
-
-print_result(dfs_result)
-
-print('Ordinary brute force:')
-ordinary_brute_force_algorithm = OrdinaryBruteForce(first_city_storage)
-ordinary_brute_result = ordinary_brute_force_algorithm.perform()
-print_result(ordinary_brute_result)
-
-print('NN algorithm:')
-nn_algorithm = NNAlgorithm(first_city_storage)
-nn_algorithm_result = nn_algorithm.perform()
-print_result(nn_algorithm_result)
-
-print('A* algorithm:')
-aa_algorithm = AAsteriskAlgorithm(first_city_storage)
-aa_algorithm_result = aa_algorithm.perform()
-print_result(aa_algorithm_result)
-
-PlotDrawer(city_storage = first_city_storage, cities = dfs_result['cities'], title = 'Brute force DFS algorithm')
-PlotDrawer(city_storage = first_city_storage, cities = bfs_result['cities'], title = 'Brute force BFS algorithm')
-PlotDrawer(city_storage = first_city_storage, cities = ordinary_brute_result['cities'], title = 'Ordinary brute force')
-PlotDrawer(city_storage = first_city_storage, cities = nn_algorithm_result['cities'], title = 'NN algorithm')
-PlotDrawer(city_storage = first_city_storage, cities = aa_algorithm_result['cities'], title = 'A* algorithm')
